a91097d2c0ec39afdbb0aa270f6be455c611a2cc
winnerlife <89582233+winnerlife@users.noreply.github.com>
2025-06-23 13:30:41 +0300

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "This is Moon"
+    return "Zeylo's Bot Live port. This site does NOT do anything"
 
 
 if __name__ == "__main__":
