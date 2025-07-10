dc4c534a2edf688214383a37b923c8c589ad286e
winnerlife <89582233+winnerlife@users.noreply.github.com>
2025-07-10 15:25:43 +0300

    cloud.sh

--- cloud.sh
+++ cloud.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-# (It's good practice to have this shebang at the top)
+this is the cloud.sh
 
 cat <<'EOF'
  _      ____  ____  _     
@@ -14,11 +13,5 @@ and is released under the "GNU v3.0 License Agreement".
 Please see < https://github.com/The-MoonTg-project/Moon-Userbot/blob/main/LICENSE >
 All rights reserved.
 EOF
-
-# --- [MODIFIED LINE] ---
-# This line now starts three processes in the correct order:
-# 1. Gunicorn (web server) in the background.
-# 2. The assistant bot in the background.
-# 3. The main userbot in the foreground (which keeps the service alive).
 
 gunicorn app:app --daemon && python3 -m assistant & && python3 main.py
